2c0223fad3f8cbce8f594e8e0393f1b2fab219f1
sayeed <sayeed0209@gmail.com>
2023-01-31 01:05:11 +0100

    Header component and link to home, shop and contact page

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import './App.css';
 import HomePage from './pages/Home/HomePage.component';
 import Shop from './pages/shop/Shop.component';
 import { Routes, Route } from 'react-router-dom';
+import Header from './components/header/Header';
 
 function App() {
 	return (
 		<div className="App">
+			<Header />
 			<Routes>
 				<Route exact path="/" element={<HomePage />} />
 				<Route exact path="/shop" element={<Shop />} />
