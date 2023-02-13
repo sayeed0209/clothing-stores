afa5e4d5ecb0754ffba13151959064aa504ca394
sayeed <sayeed0209@gmail.com>
2023-02-13 22:01:31 +0100

    removeing logger from production

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -2,7 +2,9 @@ import { createStore, applyMiddleware } from 'redux';
 import persistStore from 'redux-persist/es/persistStore';
 import logger from 'redux-logger';
 import rootReducer from '../root-reducer';
-const middleware = [logger];
-
+const middleware = [];
+if (process.env.NODE_ENV !== 'production') {
+	middleware.push(logger);
+}
 export const store = createStore(rootReducer, applyMiddleware(...middleware));
 export const persistor = persistStore(store);
