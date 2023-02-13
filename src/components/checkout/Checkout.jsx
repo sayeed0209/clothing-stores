aef38c89cf53ae971d02acc569e82a639e768ab3
sayeed <sayeed0209@gmail.com>
2023-02-13 19:49:59 +0100

    stripe checkout funcationalities added

--- src/components/checkout/Checkout.jsx
+++ src/components/checkout/Checkout.jsx
@@ -9,6 +9,12 @@ import { createStructuredSelector } from 'reselect';
 import CheckoutItem from '../checkout-items/CheckoutItem';
 import CustomButton from '../custom-button/CustomButton';
 import { clearAllCartAction } from '../../redux/cart/cartAction';
+import StripeButton from '../stripe-button/StripeButton';
+import { loadStripe } from '@stripe/stripe-js';
+import { Elements } from '@stripe/react-stripe-js';
+const stripePromise = loadStripe(
+	'pk_live_51LrnaeCsPtOBpFVsOzyrE5EtKOqMu4kg1H2jjTFF82kzrjNNDiZv1xhQxAFD4hAlSQyeYtDbxR92KZ5MaghNquqo00H6EeIdYO'
+);
 const Checkout = ({ cartItems, cartTotal, clearALlItems }) => {
 	return (
 		<div className="checkout-page">
@@ -38,6 +44,11 @@ const Checkout = ({ cartItems, cartTotal, clearALlItems }) => {
 			<CustomButton clearAll onClick={() => clearALlItems()}>
 				CLEAR ALL
 			</CustomButton>
+			<div style={{ marginTop: '50px' }}>
+				<Elements stripe={stripePromise}>
+					<StripeButton />
+				</Elements>
+			</div>
 		</div>
 	);
 };
